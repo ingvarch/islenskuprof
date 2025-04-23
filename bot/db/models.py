a12f186e0b0bf5c19c39dfd97f06edeb06ea8c84
Igor Churmeev <igor@lucinity.com>
2025-04-23 11:51:51 +0000

    Initial commit 13

--- bot/db/models.py
+++ bot/db/models.py
@@ -3,7 +3,8 @@ Database models for the Telegram bot.
 """
 import logging
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, SmallInteger
+from sqlalchemy.orm import relationship
 from bot.db.database import Base
 
 # Get logger for this module
@@ -104,3 +105,132 @@ class Name(Base):
         String representation of the name.
         """
         return f"<Name(id={self.id}, first_name={self.first_name}, last_name={self.last_name})>"
+
+
+class Job(Base):
+    """
+    Job model for storing job information.
+    """
+    __tablename__ = "jobs"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String, nullable=False)
+    workplace = Column(String, nullable=False)
+
+    def __init__(self, title, workplace):
+        """
+        Initialize a new job.
+
+        Args:
+            title: Job title
+            workplace: Workplace name
+        """
+        self.title = title
+        self.workplace = workplace
+
+    def __repr__(self):
+        """
+        String representation of the job.
+        """
+        return f"<Job(id={self.id}, title={self.title}, workplace={self.workplace})>"
+
+
+class City(Base):
+    """
+    City model for storing city information.
+    """
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False, unique=True)
+
+    def __init__(self, name):
+        """
+        Initialize a new city.
+
+        Args:
+            name: City name
+        """
+        self.name = name
+
+    def __repr__(self):
+        """
+        String representation of the city.
+        """
+        return f"<City(id={self.id}, name={self.name})>"
+
+
+class Activity(Base):
+    """
+    Activity model for storing activity information.
+    """
+    __tablename__ = "activities"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    activity = Column(String, nullable=False)
+    type = Column(String, nullable=False)
+
+    def __init__(self, activity, type):
+        """
+        Initialize a new activity.
+
+        Args:
+            activity: Activity description
+            type: Activity type
+        """
+        self.activity = activity
+        self.type = type
+
+    def __repr__(self):
+        """
+        String representation of the activity.
+        """
+        return f"<Activity(id={self.id}, activity={self.activity}, type={self.type})>"
+
+
+class Person(Base):
+    """
+    Person model for storing person information.
+    """
+    __tablename__ = "persons"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name_id = Column(Integer, ForeignKey('names.id'), nullable=False)
+    age = Column(Integer, nullable=False)
+    origin = Column(Integer, ForeignKey('cities.id'), nullable=False)
+    job_id = Column(Integer, ForeignKey('jobs.id'), nullable=False)
+    children = Column(SmallInteger, nullable=True)
+    weekend_activity = Column(String, nullable=True)
+    plan_activity = Column(String, nullable=True)
+
+    # Define relationships
+    name = relationship("Name")
+    city = relationship("City")
+    job = relationship("Job")
+
+    def __init__(self, name_id, age, origin, job_id, children=None, weekend_activity=None, plan_activity=None):
+        """
+        Initialize a new person.
+
+        Args:
+            name_id: ID of the person's name (foreign key to names table)
+            age: Person's age
+            origin: ID of the person's origin city (foreign key to cities table)
+            job_id: ID of the person's job (foreign key to jobs table)
+            children: Number of children (optional)
+            weekend_activity: Weekend activity description (optional)
+            plan_activity: Planned activity description (optional)
+        """
+        self.name_id = name_id
+        self.age = age
+        self.origin = origin
+        self.job_id = job_id
+        self.children = children
+        self.weekend_activity = weekend_activity
+        self.plan_activity = plan_activity
+
+    def __repr__(self):
+        """
+        String representation of the person.
+        """
+        return f"<Person(id={self.id}, name_id={self.name_id}, age={self.age})>"
